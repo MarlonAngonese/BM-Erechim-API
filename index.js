8d74f2cc9b18374b2f0ee02a437beb9a2d738701
MarlonAngonese <marlonangonese.dev@gmail.com>
2022-08-19 11:25:05 -0300

    Start default node project and install first dependencies

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = require('./routes')
+
+const app = express()
+
+/**
+ * Configure default port
+ */
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(":: Listening to port " + port + " ::");
+});
+
+/**
+ * Call API routes
+ */
+app.use('/api', router)
