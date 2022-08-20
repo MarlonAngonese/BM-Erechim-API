7b152adb197c965885948a05619884b079db7e52
MarlonAngonese <marlonangonese.dev@gmail.com>
2022-08-20 03:49:11 -0300

    connected to mongodb database and set .env variables

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const router = require('./routes')
+const cors = require('cors')
+const db_connection = require('./database')
 
 const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+/**
+ * Start database
+ */
+db_connection()
 
 /**
  * Configure default port
@@ -9,14 +19,10 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(":: Listening to port " + port + " ::");
+    console.log(`:: Listening to port ${port} ::`);
 });
 
 /**
  * Call API routes
  */
 app.use('/api', router)
-
-/**
- * This commit is a pipeline test
- */
